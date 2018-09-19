b286666078a124c8c0fce06b3a83b485c1880622
john144 <john144@gmail.com>
2018-09-19 09:22:37 -0500

    Fix typos. Fix element keys

--- DemoLogging.py
+++ DemoLogging.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('mymain')
 
 
 def externalFunction():
-    logger.info('Hellp from external app')
+    logger.info('Hello from external app')
     logger.info('External app sleeping 5 seconds')
     time.sleep(5)
     logger.info('External app waking up and exiting')
@@ -41,8 +41,8 @@ def main():
 
     layout =  \
         [
-            [sg.Multiline(size=(127, 30), key='log')],
-            [sg.ReadFormButton('Start', bind_return_key=True, key='start'), sg.Quit('Exit')]
+            [sg.Multiline(size=(127, 30), key='Log')],
+            [sg.ReadFormButton('Start', bind_return_key=True, key='Start'), sg.Quit('Exit')]
         ]
 
     form.LayoutAndRead(layout, non_blocking=True)
@@ -58,13 +58,13 @@ def main():
     # Loop taking in user input and querying queue
     while True:
         # Check button
-        (button, value) = form.ReadNonBlocking()
+        button, value = form.ReadNonBlocking()
 
         if button is 'Start':
             if appStarted is False:
                 threadedApp.start()
                 logger.debug('App started')
-                form.FindElement('start').Update(disabled=True)
+                form.FindElement('Start').Update(disabled=True)
                 appStarted = True
         elif value is None or button is 'EXIT':
             break
@@ -76,9 +76,9 @@ def main():
             pass
         else:
             msg = queue_handler.format(record)
-            logText = form.FindElement('log').Get()
+            logText = form.FindElement('Log').Get()
             logText += msg
-            form.FindElement('log').Update(logText)
+            form.FindElement('Log').Update(logText)
 
     exit()
 
